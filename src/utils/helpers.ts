50e3a7b3acdae024abd60d3ea8d46061a98b4d33
Michel Bégoc <michel@caravan.coop>
2024-10-15 17:58:33 -0400

    temp

---
+++ src/utils/helpers.ts
@@ -0,0 +1,8 @@
+deepMerge(...objects: object[]): object {
+    let base = objects.shift()
+    for (let obj of objects) {
+        for (let key of Object.keys(obj)) {
+            
+        }
+    }
+}
